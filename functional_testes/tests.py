d405f48bbc779fb5a1110269b4baa4a69b474848
barbara938 <barbara.brenda@academico.ifpb.edu.br>
2021-04-05 17:56:43 -0300

    Adicionando os testes

---
+++ functional_testes/tests.py
@@ -0,0 +1,48 @@
+# Ana ouviu falar de uma nova aplicação online interessante para passe de ônibus. Ela
+decidiu verificar sua homepage.
+# Ela percebeu que o título da página e o cabeçalho mencionam (BussPass).
+# Ela é convidada a escolher entre entrar no aplicativo ou fazer um cadastro.
+# Ela escolheu se cadastrar e foi levada a outra tela que o título da página e o cabeçalho
+mencionam (Cadastro), com várias caixas de texto.
+# Ao clicar com o mouse na primeira caixa de texto Ana é convidada a inserir seu nome
+e sobrenome para usar como usuário.
+# Ao clicar enter Ana é convidada a inserir seu e-mail na caixa de texto seguinte.
+# Ao clicar enter novamente, Ana é convidada a inserir seu CPF na caixa de texto
+seguinte.
+# Ao clicar enter novamente, Ana é convidada a inserir sua data de nascimento na caixa
+de texto seguinte.
+# Ao clicar enter novamente, Ana é convidada a inserir seu número de telefone na caixa
+de texto seguinte.
+# Ao clicar enter novamente, Ana é convidada a inserir sua instituição de ensino que
+frequenta na caixa de texto seguinte.
+# Ao clicar enter novamente, aparecerá o número de cadastro com 5 dígitos que o site
+formulou, Ana terá que salvar esse número para acessar o site.
+# Ana é convidada a prosseguir seu cadastro clicando na opção “Cadastrar” que está no
+final da tela.
+# Ana é levada a outra tela onde é convidada a finalizar seu cadastro criando uma senha
+de 8 dígitos ao clicar em “Continuar”.
+# Ana é convidada a inserir sua senha na caixa de texto seguinte.
+# Ana é convidada a inserir sua confirmação de senha na caixa de texto seguinte.
+# No final da tela Ana é convidada a clicar em “Ok” para finalizar seu cadastro.
+#Com o cadastro finalizado, Ana será direcionada a outra tela com título da página e
+cabeçalho (Login), com duas caixas de texto.
+# Ana é convidada a inserir seu número de cadastro na caixa de texto seguinte.
+#Ana é convidada a inserir sua senha na caixa de texto seguinte.
+#Em seguida Ana é convidada a entrar no site com seu login, clicando em “ENTRAR”
+no final da tela.#Ao entrar no site, Ana é direcionada a uma nova tela com duas caixas de texto
+(Recargas) e (Utilizações).
+#Ana será convidada a escolher entre fazer uma recarga no seu passe ou observar as
+utilizações do seu passe.
+#Ana escolheu fazer uma recarga ao clicar em “Recargas” com o mouse.
+#Ana será direcionada a outra tela com título da página e cabeçalho (Recargas), com
+quatro caixas de texto.
+# Ana é convidada a inserir seu nome de usuário caixa de texto seguinte.
+#Ao clicar enter, Ana é convidada a inserir sua data de nascimento na caixa de texto
+seguinte.
+#Ao clicar enter novamente, constará seu saldo atual na caixa de texto seguinte.
+#Ao clicar enter, Ana é convidada a inserir o valor de sua recarga na caixa de texto
+seguinte.
+#Ana é convidada a finalizar sua recarga clicando com o mouse em “OK” no final da
+tela.
+#Ana verifica seu saldo e consta o valor que ela recarregou.
+#Satisfeita ela se prepara para pegar seu ônibus de volta para casa
